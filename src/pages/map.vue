1e273a3c18698afeeeaac6f49c1e519e5e6f16a8
jesssica.hu <jessica.hu@elitesland.com>
2021-03-15 22:56:03 +0800

    init project

---
+++ src/pages/map.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'mapProject',
+    data () {
+
+      return{
+
+      };
+    },
+    components: {
+
+    }
+  }
+</script>
+
+<style lang='sass' scoped>
+
+</style>
